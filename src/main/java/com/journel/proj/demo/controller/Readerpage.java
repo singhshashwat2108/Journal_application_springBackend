144fae31a233778e37a5bc6012ae93dfe3edc1d3
singhshashwat2108 <singhshashwat2108@gmail.com>
2026-05-18 12:32:44 +0530

    second commit

--- src/main/java/com/journel/proj/demo/controller/Readerpage.java
+++ src/main/java/com/journel/proj/demo/controller/Readerpage.java
@@ -5,13 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.journel.proj.demo.entity.Journalentry;
+
 
 
 
@@ -27,10 +31,25 @@ public class Readerpage {
   }
 
   @PostMapping("/create-entries")
-  public boolean createEntry(@RequestBody Journalentry myentry){
+  public boolean createEntry(@RequestBody Journalentry myentry){          // requestbody is added to the db
     journalentry.put(myentry.getID(), myentry);
     return true;    
   }
   
+
+  @GetMapping("/id/{myid}")                                   // just like this we ahve also RequestVariable
+  public Journalentry getdata(@PathVariable long myid) {
+      return journalentry.get(myid);
+  }
+
+  @DeleteMapping("/id/{myid}")                                   // just like this we ahve also RequestVariabl
+  public Journalentry removedata(@PathVariable long myid) {
+      return journalentry.remove(myid);
+  }
+
+  @PutMapping("/id/{myid}")
+  public Journalentry updatedata(@PathVariable long myid, @RequestBody Journalentry myentry){
+    return journalentry.put(myid, myentry);
+  }
   
 }
